c2ed7501fd0e51c12620bf53148b2e4d655631cb
Dario Labrador <dariolabrador16@gmail.com>
2026-02-10 22:31:34 +0000

    feat(frontend): add style to home page text with new font

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,12 @@
 import { client } from "@/sanity/client";
 import { TEMPLATE_STATUS_QUERY } from "@/sanity/queries";
 import { BlobField } from "@/components/Blob/Blob";
+import localFont from 'next/font/local';
+        
+const cooper = localFont({
+  src: './fonts/cooper-black-cdnfonts/coopbl.ttf',
+  display: 'swap',
+})
 
 export default async function HomePage() {
   return (
@@ -9,7 +15,29 @@ export default async function HomePage() {
       style={{ background: '#da8da0' }}
     >
       <BlobField />
-      <h1 className="relative z-10 text-6xl font-bold text-white mix-blend-difference">
+      <h1
+       className={
+        `relative z-10 
+      
+        text-7xl 
+      
+        font-bold
+      
+        italic
+      
+        text-pink-300
+        
+        [-webkit-text-stroke:3px_black]
+        
+        hover:[-webkit-text-stroke:3.5px_black]
+
+        transition-all duration-300 ease-in-out
+        
+        cursor-default
+        
+        ${cooper.className}`}>
+        {/* ^^ had to remove mix-blend-difference bc pink wasnt working */}
+        
         Template Ready
       </h1>
     </main>
